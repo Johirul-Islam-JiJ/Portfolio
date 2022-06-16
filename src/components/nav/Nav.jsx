24db8f5ddab5e4e1fad4ca432a7c00164ca21cae
unknown <you@example.com>
2022-06-16 17:16:57 +0600

    Floating NavBar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,17 +5,19 @@ import {BsBookHalf} from 'react-icons/bs'
 import {FaUserGraduate} from 'react-icons/fa'
 import {CgProfile} from 'react-icons/cg'
 import {RiContactsBook2Line} from 'react-icons/ri'
+import { useState } from 'react'
 
-const nav = () => {
+const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><GrHomeRounded/></a>
-      <a href="#about"><FaUserGraduate/></a>
-      <a href="#experience"><BsBookHalf/></a>
-      <a href="#portfolio"><CgProfile/></a>
-      <a href="#contact"><RiContactsBook2Line/></a>
+      <a href="#" onClick={()=>setActiveNav('#')} className = {activeNav === '#' ? 'active':''}><GrHomeRounded/></a>
+      <a href="#about" onClick={()=>setActiveNav('about')} className={activeNav==='about' ? 'active':''}><FaUserGraduate/></a>
+      <a href="#experience" onClick={()=>setActiveNav('experience')} className={activeNav==='experience' ? 'active':''}><BsBookHalf/></a>
+      <a href="#portfolio" onClick={()=>setActiveNav('portfolio')} className={activeNav==='portfolio' ? 'active':''}><CgProfile/></a>
+      <a href="#contact" onClick={()=>setActiveNav('contact')} className={activeNav==='contact' ? 'active':''}><RiContactsBook2Line/></a>
     </nav>
   )
 }
 
-export default nav
+export default Nav
