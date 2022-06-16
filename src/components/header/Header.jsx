7dc57a73376766cd454481ef54c0485a800623f5
unknown <you@example.com>
2022-06-16 18:17:51 +0600

    header Background

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/myself.jpg'
+import ME from '../../assets/smile.png'
 import HeaderSocial from './HeaderSocial';
 
 const Header = () => {
