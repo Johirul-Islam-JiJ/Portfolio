26e9805e0bba9326b911dafb18c5bde80f73630b
unknown <you@example.com>
2022-06-19 20:37:40 +0600

    nav bar update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './nav.css'
-import {GrHomeRounded} from 'react-icons/gr'
+import {MdHome} from 'react-icons/md'
 import {BsBookHalf} from 'react-icons/bs'
 import {FaUserGraduate} from 'react-icons/fa'
 import {CgProfile} from 'react-icons/cg'
@@ -11,7 +11,7 @@ const Nav = () => {
   const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={()=>setActiveNav('#')} className = {activeNav === '#' ? 'active':''}><GrHomeRounded/></a>
+      <a href="#" onClick={()=>setActiveNav('#')} className = {activeNav === '#' ? 'active':''}><MdHome/></a>
       <a href="#about" onClick={()=>setActiveNav('about')} className={activeNav==='about' ? 'active':''}><FaUserGraduate/></a>
       <a href="#experience" onClick={()=>setActiveNav('experience')} className={activeNav==='experience' ? 'active':''}><BsBookHalf/></a>
       <a href="#portfolio" onClick={()=>setActiveNav('portfolio')} className={activeNav==='portfolio' ? 'active':''}><CgProfile/></a>
